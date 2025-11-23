88e13e308e2878340030d3422c96bd4ed4d5821c
นฤภาส วรวรัตน์ <nateeronron@gmail.com>
2025-11-23 14:22:42 +0700

    start

---
+++ BotGridV1/Controllers/RunController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BotGridV1.Controllers
+{
+    [Route("api/[controller]/[Action]")]
+    [ApiController]
+    public class RunController : ControllerBase
+    {
+
+        [HttpGet("/")]
+        public async Task<IActionResult> Run()
+        {
+            return Ok("Ok Run .Net C# v.8.0");
+        }
+    }
+}
